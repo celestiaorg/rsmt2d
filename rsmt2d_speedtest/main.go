622fdad04d7a1b45ac3478e320f7d545d0c889b6
Mustafa Al-Bassam <mus@musalbas.com>
2018-08-15 17:47:28 +0100

    Add speed tests.

---
+++ rsmt2d_speedtest/main.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+    "fmt"
+    "time"
+    "crypto/rand"
+
+    "github.com/musalbas/rsmt2d"
+)
+
+func main() {
+    widths := []uint{64, 128}
+    repeats := 10
+
+    fmt.Println("Square width\t Chunk size\t Average time to encode (s)")
+
+    for _, width := range widths {
+        var runs []float64
+        for i := 0; i < repeats; i++ {
+            start := time.Now()
+            _, err := rsmt2d.ComputeExtendedDataSquare(generateRandomSquare(width, 256))
+            if err != nil {
+                panic(err)
+            }
+            runs = append(runs, time.Since(start).Seconds())
+        }
+        fmt.Println(width, "\t\t", 256, "\t\t", mean(runs))
+    }
+}
+
+func generateRandomSquare(width uint, chunkSize uint) [][]byte {
+    chunks := make([][]byte, width*width)
+
+    for i := 0; i < len(chunks); i++ {
+        chunks[i] = make([]byte, chunkSize)
+        rand.Read(chunks[i])
+    }
+
+    return chunks
+}
+
+func mean(values []float64) float64 {
+    sum := float64(0)
+    for _, value := range values {
+        sum += value
+    }
+    return sum/float64(len(values))
+}
