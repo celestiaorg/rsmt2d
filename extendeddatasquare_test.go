d1e5ae800b0eb698b1d874e5d71c2ab4455949eb
evan-forbes <evan.samuel.forbes@gmail.com>
2021-01-20 11:29:43 -0600

    fix from int to codec type

--- extendeddatasquare_test.go
+++ extendeddatasquare_test.go
@@ -32,13 +32,13 @@ var dump *ExtendedDataSquare
 
 // BenchmarkExtension benchmarks extending datasquares sizes 4-128 using all supported codecs
 func BenchmarkExtension(b *testing.B) {
-	var codecTypes []string
+	var codecTypes []CodecType
 	for codec := range codecs {
-		codecTypes = append(codecTypes, codec.String())
+		codecTypes = append(codecTypes, codec)
 	}
 	for i := 4; i < 129; i *= 2 {
 		fmt.Printf("%s Square Width: %d (shares) (extended = %d)\n", benchmarkDivider, i, i*2)
-		for _codecType := range codecTypes {
+		for _, _codecType := range codecTypes {
 			square := genRandDS(i)
 			b.Run(
 				fmt.Sprintf("%s size %d", _codecType, i),
