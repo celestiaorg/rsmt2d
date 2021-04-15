bcb6e50e0b2b8a1fe501896c2ba6fd781d78e9a7
evan-forbes <evan.samuel.forbes@gmail.com>
2021-04-15 16:50:23 -0500

    refactor codec api

--- extendeddatasquare.go
+++ extendeddatasquare.go
@@ -10,17 +10,12 @@ import (
 type ExtendedDataSquare struct {
 	*dataSquare
 	originalDataWidth uint
-	codec             CodecType
 }
 
 // ComputeExtendedDataSquare computes the extended data square for some chunks of data.
-func ComputeExtendedDataSquare(data [][]byte, codecType CodecType, treeCreatorFn TreeConstructorFn) (*ExtendedDataSquare, error) {
-	if codec, ok := codecs[codecType]; !ok {
-		return nil, errors.New("unsupported codecType")
-	} else {
-		if len(data) > codec.maxChunks() {
-			return nil, errors.New("number of chunks exceeds the maximum")
-		}
+func ComputeExtendedDataSquare(data [][]byte, codec Codec, treeCreatorFn TreeConstructorFn) (*ExtendedDataSquare, error) {
+	if len(data) > codec.maxChunks() {
+		return nil, errors.New("number of chunks exceeds the maximum")
 	}
 
 	ds, err := newDataSquare(data, treeCreatorFn)
@@ -28,8 +23,8 @@ func ComputeExtendedDataSquare(data [][]byte, codecType CodecType, treeCreatorFn
 		return nil, err
 	}
 
-	eds := ExtendedDataSquare{dataSquare: ds, codec: codecType}
-	err = eds.erasureExtendSquare()
+	eds := ExtendedDataSquare{dataSquare: ds}
+	err = eds.erasureExtendSquare(codec)
 	if err != nil {
 		return nil, err
 	}
@@ -38,20 +33,17 @@ func ComputeExtendedDataSquare(data [][]byte, codecType CodecType, treeCreatorFn
 }
 
 // ImportExtendedDataSquare imports an extended data square, represented as flattened chunks of data.
-func ImportExtendedDataSquare(data [][]byte, codecType CodecType, treeCreatorFn TreeConstructorFn) (*ExtendedDataSquare, error) {
-	if codec, ok := codecs[codecType]; !ok {
-		return nil, errors.New("unsupported codecType")
-	} else {
-		if len(data) > 4*codec.maxChunks() {
-			return nil, errors.New("number of chunks exceeds the maximum")
-		}
+func ImportExtendedDataSquare(data [][]byte, codec Codec, treeCreatorFn TreeConstructorFn) (*ExtendedDataSquare, error) {
+	if len(data) > 4*codec.maxChunks() {
+		return nil, errors.New("number of chunks exceeds the maximum")
 	}
+
 	ds, err := newDataSquare(data, treeCreatorFn)
 	if err != nil {
 		return nil, err
 	}
 
-	eds := ExtendedDataSquare{dataSquare: ds, codec: codecType}
+	eds := ExtendedDataSquare{dataSquare: ds}
 	if eds.width%2 != 0 {
 		return nil, errors.New("square width must be even")
 	}
@@ -61,7 +53,7 @@ func ImportExtendedDataSquare(data [][]byte, codecType CodecType, treeCreatorFn 
 	return &eds, nil
 }
 
-func (eds *ExtendedDataSquare) erasureExtendSquare() error {
+func (eds *ExtendedDataSquare) erasureExtendSquare(codec Codec) error {
 	eds.originalDataWidth = eds.width
 	if err := eds.extendSquare(eds.width, bytes.Repeat([]byte{0}, int(eds.chunkSize))); err != nil {
 		return err
@@ -82,7 +74,7 @@ func (eds *ExtendedDataSquare) erasureExtendSquare() error {
 	//  -------
 	for i := uint(0); i < eds.originalDataWidth; i++ {
 		// Extend horizontally
-		shares, err = Encode(eds.rowSlice(i, 0, eds.originalDataWidth), eds.codec)
+		shares, err = codec.Encode(eds.rowSlice(i, 0, eds.originalDataWidth))
 		if err != nil {
 			return err
 		}
@@ -91,7 +83,7 @@ func (eds *ExtendedDataSquare) erasureExtendSquare() error {
 		}
 
 		// Extend vertically
-		shares, err = Encode(eds.columnSlice(0, i, eds.originalDataWidth), eds.codec)
+		shares, err = codec.Encode(eds.columnSlice(0, i, eds.originalDataWidth))
 		if err != nil {
 			return err
 		}
@@ -112,7 +104,7 @@ func (eds *ExtendedDataSquare) erasureExtendSquare() error {
 	//  ------- -------
 	for i := eds.originalDataWidth; i < eds.width; i++ {
 		// Extend horizontally
-		shares, err = Encode(eds.rowSlice(i, 0, eds.originalDataWidth), eds.codec)
+		shares, err = codec.Encode(eds.rowSlice(i, 0, eds.originalDataWidth))
 		if err != nil {
 			return err
 		}
@@ -124,7 +116,7 @@ func (eds *ExtendedDataSquare) erasureExtendSquare() error {
 	return nil
 }
 
-func (eds *ExtendedDataSquare) deepCopy() (ExtendedDataSquare, error) {
-	eds, err := ImportExtendedDataSquare(eds.flattened(), eds.codec, eds.createTreeFn)
+func (eds *ExtendedDataSquare) deepCopy(codec Codec) (ExtendedDataSquare, error) {
+	eds, err := ImportExtendedDataSquare(eds.flattened(), codec, eds.createTreeFn)
 	return *eds, err
 }
