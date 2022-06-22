35f8231bdff5f055167b3a428b980c4baee0edf6
Ryan <ryanford@poluglottos.com>
2022-06-22 12:35:49 +0200

    Hierarchical tests in extendeddatacrossword_test and datasquare_test

--- extendeddatacrossword_test.go
+++ extendeddatacrossword_test.go
@@ -20,142 +20,153 @@ type PseudoFraudProof struct {
 
 func TestRepairExtendedDataSquare(t *testing.T) {
 	for codecName, codec := range codecs {
+		t.Run(codecName, func(t *testing.T) {
+			bufferSize := 64
+			ones := bytes.Repeat([]byte{1}, bufferSize)
+			twos := bytes.Repeat([]byte{2}, bufferSize)
+			threes := bytes.Repeat([]byte{3}, bufferSize)
+			fours := bytes.Repeat([]byte{4}, bufferSize)
 
-		bufferSize := 64
-		ones := bytes.Repeat([]byte{1}, bufferSize)
-		twos := bytes.Repeat([]byte{2}, bufferSize)
-		threes := bytes.Repeat([]byte{3}, bufferSize)
-		fours := bytes.Repeat([]byte{4}, bufferSize)
-
-		original, err := ComputeExtendedDataSquare([][]byte{
-			ones, twos,
-			threes, fours,
-		}, codec, NewDefaultTree)
-		if err != nil {
-			panic(err)
-		}
-
-		rowRoots := original.RowRoots()
-		colRoots := original.ColRoots()
-
-		flattened := original.Flattened()
-		flattened[0], flattened[2], flattened[3] = nil, nil, nil
-		flattened[4], flattened[5], flattened[6], flattened[7] = nil, nil, nil, nil
-		flattened[8], flattened[9], flattened[10] = nil, nil, nil
-		flattened[12], flattened[13] = nil, nil
-
-		// Re-import the data square.
-		eds, err := ImportExtendedDataSquare(flattened, codec, NewDefaultTree)
-		if err != nil {
-			t.Errorf("ImportExtendedDataSquare failed: %v", err)
-		}
-
-		err = eds.Repair(rowRoots, colRoots)
-		if err != nil {
-			t.Errorf("unexpected err while repairing data square: %v, codec: :%s", err, codecName)
-		} else {
-			assert.Equal(t, original.GetCell(0, 0), ones)
-			assert.Equal(t, original.GetCell(0, 1), twos)
-			assert.Equal(t, original.GetCell(1, 0), threes)
-			assert.Equal(t, original.GetCell(1, 1), fours)
-		}
-
-		flattened = original.Flattened()
-		flattened[0], flattened[2], flattened[3] = nil, nil, nil
-		flattened[4], flattened[5], flattened[6], flattened[7] = nil, nil, nil, nil
-		flattened[8], flattened[9], flattened[10] = nil, nil, nil
-		flattened[12], flattened[13], flattened[14] = nil, nil, nil
-
-		// Re-import the data square.
-		eds, err = ImportExtendedDataSquare(flattened, codec, NewDefaultTree)
-		if err != nil {
-			t.Errorf("ImportExtendedDataSquare failed: %v", err)
-		}
-
-		err = eds.Repair(rowRoots, colRoots)
-		if err == nil {
-			t.Errorf("did not return an error on trying to repair an unrepairable square")
-		}
-		var corrupted ExtendedDataSquare
-		corrupted, err = original.deepCopy(codec)
-		if err != nil {
-			t.Fatalf("unexpected err while copying original data: %v, codec: :%s", err, codecName)
-		}
-		corruptChunk := bytes.Repeat([]byte{66}, bufferSize)
-		corrupted.setCell(0, 0, corruptChunk)
-		err = corrupted.Repair(rowRoots, colRoots)
-		if err == nil {
-			t.Errorf("did not return an error on trying to repair a square with bad roots")
-		}
-
-		corrupted, err = original.deepCopy(codec)
-		if err != nil {
-			t.Fatalf("unexpected err while copying original data: %v, codec: :%s", err, codecName)
-		}
-		corrupted.setCell(0, 0, corruptChunk)
-		err = corrupted.Repair(corrupted.getRowRoots(), corrupted.getColRoots())
-		var byzData *ErrByzantineData
-		if !errors.As(err, &byzData) {
-			// due to parallelisation, the ErrByzantineData axis may be either row or col
-			t.Errorf("did not return a ErrByzantineData for a bad row or col; got: %v", err)
-		}
-		// Construct the fraud proof
-		fraudProof := PseudoFraudProof{0, byzData.Index, byzData.Shares}
-		// Verify the fraud proof
-		// TODO in a real fraud proof, also verify Merkle proof for each non-nil share.
-		rebuiltShares, err := codec.Decode(fraudProof.Shares)
-		if err != nil {
-			t.Errorf("could not decode fraud proof shares; got: %v", err)
-		}
-		root := corrupted.computeSharesRoot(rebuiltShares, byzData.Axis, fraudProof.Index)
-		if bytes.Equal(root, corrupted.getRowRoot(fraudProof.Index)) {
-			// If the roots match, then the fraud proof should be for invalid erasure coding.
-			parityShares, err := codec.Encode(rebuiltShares[0:corrupted.originalDataWidth])
+			original, err := ComputeExtendedDataSquare([][]byte{
+				ones, twos,
+				threes, fours,
+			}, codec, NewDefaultTree)
 			if err != nil {
-				t.Errorf("could not encode fraud proof shares; %v", fraudProof)
+				panic(err)
 			}
-			startIndex := len(rebuiltShares) - int(corrupted.originalDataWidth)
-			if bytes.Equal(flattenChunks(parityShares), flattenChunks(rebuiltShares[startIndex:])) {
-				t.Errorf("invalid fraud proof %v", fraudProof)
-			}
-		}
 
-		corrupted, err = original.deepCopy(codec)
-		if err != nil {
-			t.Fatalf("unexpected err while copying original data: %v, codec: :%s", err, codecName)
-		}
-		corrupted.setCell(0, 3, corruptChunk)
-		err = corrupted.Repair(corrupted.getRowRoots(), corrupted.getColRoots())
-		if !errors.As(err, &byzData) {
-			// due to parallelisation, the ErrByzantineData axis may be either row or col
-			t.Errorf("did not return a ErrByzantineData for a bad row or col; got %v", err)
-		}
+			rowRoots := original.RowRoots()
+			colRoots := original.ColRoots()
 
-		corrupted, err = original.deepCopy(codec)
-		if err != nil {
-			t.Fatalf("unexpected err while copying original data: %v, codec: :%s", err, codecName)
-		}
-		corrupted.setCell(0, 0, corruptChunk)
-		corrupted.setCell(0, 1, nil)
-		corrupted.setCell(0, 2, nil)
-		corrupted.setCell(0, 3, nil)
-		err = corrupted.Repair(corrupted.getRowRoots(), corrupted.getColRoots())
-		if !errors.As(err, &byzData) || byzData.Axis != Col {
-			t.Errorf("did not return a ErrByzantineData for a bad column; got %v", err)
-		}
-		corrupted, err = original.deepCopy(codec)
-		if err != nil {
-			t.Fatalf("unexpected err while copying original data: %v, codec: :%s", err, codecName)
-		}
-		corrupted.setCell(3, 0, corruptChunk)
-		corrupted.setCell(0, 1, nil)
-		corrupted.setCell(0, 2, nil)
-		corrupted.setCell(0, 3, nil)
-		err = corrupted.Repair(corrupted.getRowRoots(), corrupted.getColRoots())
-		if !errors.As(err, &byzData) {
-			// due to parallelisation, the ErrByzantineData axis may be either row or col
-			t.Errorf("did not return a ErrByzantineData for a bad col or row; got %v", err)
-		}
+			t.Run("MaximumErasures", func(t *testing.T) {
+				flattened := original.Flattened()
+				flattened[0], flattened[2], flattened[3] = nil, nil, nil
+				flattened[4], flattened[5], flattened[6], flattened[7] = nil, nil, nil, nil
+				flattened[8], flattened[9], flattened[10] = nil, nil, nil
+				flattened[12], flattened[13] = nil, nil
+
+				// Re-import the data square.
+				eds, err := ImportExtendedDataSquare(flattened, codec, NewDefaultTree)
+				if err != nil {
+					t.Errorf("ImportExtendedDataSquare failed: %v", err)
+				}
+
+				err = eds.Repair(rowRoots, colRoots)
+				if err != nil {
+					t.Errorf("unexpected err while repairing data square: %v, codec: :%s", err, codecName)
+				} else {
+					assert.Equal(t, original.GetCell(0, 0), ones)
+					assert.Equal(t, original.GetCell(0, 1), twos)
+					assert.Equal(t, original.GetCell(1, 0), threes)
+					assert.Equal(t, original.GetCell(1, 1), fours)
+				}
+			})
+			t.Run("Unrepairable", func(t *testing.T) {
+				flattened := original.Flattened()
+				flattened[0], flattened[2], flattened[3] = nil, nil, nil
+				flattened[4], flattened[5], flattened[6], flattened[7] = nil, nil, nil, nil
+				flattened[8], flattened[9], flattened[10] = nil, nil, nil
+				flattened[12], flattened[13], flattened[14] = nil, nil, nil
+
+				// Re-import the data square.
+				eds, err := ImportExtendedDataSquare(flattened, codec, NewDefaultTree)
+				if err != nil {
+					t.Errorf("ImportExtendedDataSquare failed: %v", err)
+				}
+
+				err = eds.Repair(rowRoots, colRoots)
+				if err == nil {
+					t.Errorf("did not return an error on trying to repair an unrepairable square")
+				}
+			})
+			t.Run("CorruptedEDS", func(t *testing.T) {
+				var corrupted ExtendedDataSquare
+				var byzData *ErrByzantineData
+				corrupted, err = original.deepCopy(codec)
+				if err != nil {
+					t.Fatalf("unexpected err while copying original data: %v, codec: :%s", err, codecName)
+				}
+				corruptChunk := bytes.Repeat([]byte{66}, bufferSize)
+				corrupted.setCell(0, 0, corruptChunk)
+				err = corrupted.Repair(rowRoots, colRoots)
+				if err == nil {
+					t.Errorf("did not return an error on trying to repair a square with bad roots")
+				}
+				t.Run("BadRowInOriginalData", func(t *testing.T) {
+					corrupted, err = original.deepCopy(codec)
+					if err != nil {
+						t.Fatalf("unexpected err while copying original data: %v, codec: :%s", err, codecName)
+					}
+					corrupted.setCell(0, 0, corruptChunk)
+					err = corrupted.Repair(corrupted.getRowRoots(), corrupted.getColRoots())
+					if !errors.As(err, &byzData) {
+						// due to parallelisation, the ErrByzantineData axis may be either row or col
+						t.Errorf("did not return a ErrByzantineData for a bad row or col; got: %v", err)
+					}
+					// Construct the fraud proof
+					fraudProof := PseudoFraudProof{0, byzData.Index, byzData.Shares}
+					// Verify the fraud proof
+					// TODO in a real fraud proof, also verify Merkle proof for each non-nil share.
+					rebuiltShares, err := codec.Decode(fraudProof.Shares)
+					if err != nil {
+						t.Errorf("could not decode fraud proof shares; got: %v", err)
+					}
+					root := corrupted.computeSharesRoot(rebuiltShares, byzData.Axis, fraudProof.Index)
+					if bytes.Equal(root, corrupted.getRowRoot(fraudProof.Index)) {
+						// If the roots match, then the fraud proof should be for invalid erasure coding.
+						parityShares, err := codec.Encode(rebuiltShares[0:corrupted.originalDataWidth])
+						if err != nil {
+							t.Errorf("could not encode fraud proof shares; %v", fraudProof)
+						}
+						startIndex := len(rebuiltShares) - int(corrupted.originalDataWidth)
+						if bytes.Equal(flattenChunks(parityShares), flattenChunks(rebuiltShares[startIndex:])) {
+							t.Errorf("invalid fraud proof %v", fraudProof)
+						}
+					}
+				})
+				t.Run("BadRowInExtendedData", func(t *testing.T) {
+					corrupted, err = original.deepCopy(codec)
+					if err != nil {
+						t.Fatalf("unexpected err while copying original data: %v, codec: :%s", err, codecName)
+					}
+					corrupted.setCell(0, 3, corruptChunk)
+					err = corrupted.Repair(corrupted.getRowRoots(), corrupted.getColRoots())
+					if !errors.As(err, &byzData) {
+						// due to parallelisation, the ErrByzantineData axis may be either row or col
+						t.Errorf("did not return a ErrByzantineData for a bad row; got %v", err)
+					}
+				})
+				t.Run("BadColumnInOriginalData", func(t *testing.T) {
+					corrupted, err = original.deepCopy(codec)
+					if err != nil {
+						t.Fatalf("unexpected err while copying original data: %v, codec: :%s", err, codecName)
+					}
+					corrupted.setCell(0, 0, corruptChunk)
+					corrupted.setCell(0, 1, nil)
+					corrupted.setCell(0, 2, nil)
+					corrupted.setCell(0, 3, nil)
+					err = corrupted.Repair(corrupted.getRowRoots(), corrupted.getColRoots())
+					if !errors.As(err, &byzData) || byzData.Axis != Col {
+						t.Errorf("did not return a ErrByzantineData for a bad column; got %v", err)
+					}
+				})
+				t.Run("BadColumnInExtendedData", func(t *testing.T) {
+					corrupted, err = original.deepCopy(codec)
+					if err != nil {
+						t.Fatalf("unexpected err while copying original data: %v, codec: :%s", err, codecName)
+					}
+					corrupted.setCell(3, 0, corruptChunk)
+					corrupted.setCell(0, 1, nil)
+					corrupted.setCell(0, 2, nil)
+					corrupted.setCell(0, 3, nil)
+					err = corrupted.Repair(corrupted.getRowRoots(), corrupted.getColRoots())
+					if !errors.As(err, &byzData) {
+						// due to parallelisation, the ErrByzantineData axis may be either row or col
+						t.Errorf("did not return a ErrByzantineData for a bad col or row; got %v", err)
+					}
+				})
+			})
+		})
 	}
 }
 
