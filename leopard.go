4775d1e3c565b917146dc679aaef5295b4da53ba
Rahul Ghangas <rahulghangas0@gmail.com>
2022-10-05 02:30:39 +1100

    chore: remove different type defs for ff8 and ff16 and create single leoRSCodec instead

--- leopard.go
+++ leopard.go
@@ -1,36 +1,29 @@
 package rsmt2d
 
 import (
-	"github.com/celestiaorg/rsmt2d/utils"
 	"github.com/klauspost/reedsolomon"
 )
 
-var _ Codec = leoRSFF8Codec{}
-var _ Codec = leoRSFF16Codec{}
+var _ Codec = leoRSCodec{}
 
-var encoderCache = utils.NewDoubleCache[reedsolomon.Encoder](utils.DefaultDoubleCacheOptions())
+var encoderCache = newDoubleCache[reedsolomon.Encoder](defaultDoubleCacheOptions())
 
 func init() {
-	registerCodec(LeopardFF8, newLeoRSFF8Codec())
-	registerCodec(LeopardFF16, newLeoRSFF16Codec())
+	registerCodec(Leopard, newLeoRSCodec())
 }
 
-type leoRSFF8Codec struct{}
+type leoRSCodec struct{}
 
-func (l leoRSFF8Codec) Encode(data [][]byte) ([][]byte, error) {
-	return encode(data)
-}
-
-func encode(data [][]byte) ([][]byte, error) {
+func (l leoRSCodec) Encode(data [][]byte) ([][]byte, error) {
 	dataLen := len(data)
-	enc, ok := encoderCache.Query(dataLen)
+	enc, ok := encoderCache.query(dataLen)
 	if !ok {
 		var err error
 		enc, err = reedsolomon.New(dataLen, dataLen, reedsolomon.WithLeopardGF(true))
 		if err != nil {
 			return nil, err
 		}
-		encoderCache.Insert(dataLen, enc)
+		encoderCache.insert(dataLen, enc)
 	}
 
 	shards := make([][]byte, dataLen*2)
@@ -44,47 +37,25 @@ func encode(data [][]byte) ([][]byte, error) {
 	return shards[dataLen:], nil
 }
 
-func (l leoRSFF8Codec) Decode(data [][]byte) ([][]byte, error) {
-	return decode(data)
-}
-
-func decode(data [][]byte) ([][]byte, error) {
+func (l leoRSCodec) Decode(data [][]byte) ([][]byte, error) {
 	half := len(data) / 2
-	enc, ok := encoderCache.Query(half)
+	enc, ok := encoderCache.query(half)
 	var err error
 	if !ok {
 		enc, err = reedsolomon.New(half, half, reedsolomon.WithLeopardGF(true))
 		if err != nil {
 			return nil, err
 		}
-		encoderCache.Insert(half, enc)
+		encoderCache.insert(half, enc)
 	}
 	err = enc.Reconstruct(data)
 	return data, err
 }
 
-func (l leoRSFF8Codec) maxChunks() int {
-	return 128 * 128
-}
-
-func newLeoRSFF8Codec() leoRSFF8Codec {
-	return leoRSFF8Codec{}
-}
-
-type leoRSFF16Codec struct{}
-
-func (leo leoRSFF16Codec) Encode(data [][]byte) ([][]byte, error) {
-	return encode(data)
-}
-
-func (leo leoRSFF16Codec) Decode(data [][]byte) ([][]byte, error) {
-	return decode(data)
-}
-
-func (leo leoRSFF16Codec) maxChunks() int {
+func (l leoRSCodec) maxChunks() int {
 	return 32768 * 32768
 }
 
-func newLeoRSFF16Codec() leoRSFF16Codec {
-	return leoRSFF16Codec{}
+func newLeoRSCodec() leoRSCodec {
+	return leoRSCodec{}
 }
