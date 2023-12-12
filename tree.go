80f81c418806275a787c629dda5dbe5379fdcc4c
sontrinh16 <trinhleson2000@gmail.com>
2023-12-12 12:07:29 +0700

    refator default tree to seperated file

--- tree.go
+++ tree.go
@@ -1,11 +1,5 @@
 package rsmt2d
 
-import (
-	"crypto/sha256"
-
-	"github.com/celestiaorg/merkletree"
-)
-
 // TreeConstructorFn creates a fresh Tree instance to be used as the Merkle tree
 // inside of rsmt2d.
 type TreeConstructorFn = func(axis Axis, index uint) Tree
@@ -20,35 +14,4 @@ type SquareIndex struct {
 type Tree interface {
 	Push(data []byte) error
 	Root() ([]byte, error)
-}
-
-var _ Tree = &DefaultTree{}
-
-type DefaultTree struct {
-	*merkletree.Tree
-	leaves [][]byte
-	root   []byte
-}
-
-func NewDefaultTree(_ Axis, _ uint) Tree {
-	return &DefaultTree{
-		Tree:   merkletree.New(sha256.New()),
-		leaves: make([][]byte, 0, 128),
-	}
-}
-
-func (d *DefaultTree) Push(data []byte) error {
-	// ignore the idx, as this implementation doesn't need that info
-	d.leaves = append(d.leaves, data)
-	return nil
-}
-
-func (d *DefaultTree) Root() ([]byte, error) {
-	if d.root == nil {
-		for _, l := range d.leaves {
-			d.Tree.Push(l)
-		}
-		d.root = d.Tree.Root()
-	}
-	return d.root, nil
 }
