98c9f3a61b2fbb1cb44d2130743f080bfde3c8ef
sontrinh16 <trinhleson2000@gmail.com>
2023-12-15 20:59:05 +0700

    use sync map

--- tree.go
+++ tree.go
@@ -3,6 +3,7 @@ package rsmt2d
 import (
 	"fmt"
 	"reflect"
+	"sync"
 )
 
 // TreeConstructorFn creates a fresh Tree instance to be used as the Merkle tree
@@ -21,36 +22,56 @@ type Tree interface {
 	Root() ([]byte, error)
 }
 
-// trees is a global map used for keeping track of registered tree constructors for JSON serialization
+// treeFns is a global map used for keeping track of registered tree constructors for JSON serialization
 // The keys of this map should be kebab cased. E.g. "default-tree"
-var trees = make(map[string]TreeConstructorFn)
+var treeFns = sync.Map{}
 
 // Must be called in the init function
 func RegisterTree(treeName string, treeConstructor TreeConstructorFn) error {
-	if trees[treeName] != nil {
+	if _, ok := treeFns.Load(treeName); ok {
 		return fmt.Errorf("%s already registered", treeName)
 	}
-	trees[treeName] = treeConstructor
+
+	treeFns.Store(treeName, treeConstructor)
 
 	return nil
 }
 
 // Get tree constructor function by tree name from the global map
 func TreeFn(treeName string) (TreeConstructorFn, error) {
-	if trees[treeName] == nil {
+	var treeFn TreeConstructorFn
+	v, ok := treeFns.Load(treeName)
+	if !ok {
 		return nil, fmt.Errorf("%s not registered yet", treeName)
+
+	}
+	treeFn, ok = v.(TreeConstructorFn)
+	if !ok {
+		return nil, fmt.Errorf("key %s has invalid interface", treeName)
 	}
 
-	return trees[treeName], nil
+	return treeFn, nil
 }
 
 func getTreeNameFromConstructorFn(treeConstructor TreeConstructorFn) string {
 	key := ""
-	for k, v := range trees {
-		if reflect.DeepEqual(reflect.ValueOf(v), reflect.ValueOf(treeConstructor)) {
-			key = k
+	treeFns.Range(func(k, v interface{}) bool {
+		keyString, ok := k.(string)
+		if !ok {
+			return false
 		}
-	}
+		treeFn, ok := v.(TreeConstructorFn)
+		if !ok {
+			return false
+		}
+
+		if reflect.DeepEqual(reflect.ValueOf(treeFn), reflect.ValueOf(treeConstructor)) {
+			key = keyString
+			return false
+		}
+
+		return true
+	})
 
 	return key
 }
