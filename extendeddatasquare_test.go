celestiaorg/rsmt2d

commit e95575e4423594ed01d40e769debd5345d7349b2

api: element-wise copy on eds.Row and eds.Col (#100)
* api: element-wise copy on eds.Row and eds.Col

* fix: col misuse caught by leopard tests

extendeddatasquare.go
@@ -133,9 +133,13 @@ func (eds *ExtendedDataSquare) deepCopy(codec Codec) (ExtendedDataSquare, error)
 // Col returns a column slice.
 // This slice is a copy of the internal column slice.
 func (eds *ExtendedDataSquare) Col(y uint) [][]byte {
-	s := make([][]byte, eds.width)
-	copy(s, eds.colSlice(0, y, eds.width))
-	return s
+	col := make([][]byte, eds.width)
+	original := eds.col(y)
+	for i, cell := range original {
+		col[i] = make([]byte, eds.chunkSize)
+		copy(col[i], cell)
+	}
+	return col
 }
 
 // ColRoots returns the Merkle roots of all the columns in the square.
@@ -146,9 +150,13 @@ func (eds *ExtendedDataSquare) ColRoots() [][]byte {
 // Row returns a row slice.
 // This slice is a copy of the internal row slice.
 func (eds *ExtendedDataSquare) Row(x uint) [][]byte {
-	s := make([][]byte, eds.width)
-	copy(s, eds.rowSlice(x, 0, eds.width))
-	return s
+	row := make([][]byte, eds.width)
+	original := eds.row(x)
+	for i, cell := range original {
+		row[i] = make([]byte, eds.chunkSize)
+		copy(row[i], cell)
+	}
+	return row
 }
 
 // RowRoots returns the Merkle roots of all the rows in the square.

extendeddatacrossword.go
@@ -124,7 +124,6 @@ func (eds *ExtendedDataSquare) solveCrosswordRow(
 	vectorData := eds.row(uint(r))
 	for c := 0; c < int(eds.width); c++ {
 		shares[c] = vectorData[c]
-
 	}
 
 	isExtendedPartIncomplete := !eds.rowRangeNoMissingData(uint(r), eds.originalDataWidth, eds.width)

commit 18f79c26b0865d4bba5799c5e5bbd25b7da4fce8

fix: deepcopy filliing nil data with zeroes (#310)
Corrects the bug introduced by
https://github.com/celestiaorg/rsmt2d/pull/100. Currently, if
`deepcopy()` encounters nil inner slice values, it creates a slice of 0
length, which is incorrect. This PR addresses the issue. The problem
becomes evident when eds is partially computed, resulting in Row() or
Col() methods returning invalid copies.

On the side note, it is technically breaking public API change. But the
nature of the bug suggests noone relied on existing misbehaviour before.

extendeddatasquare.go
@@ -267,6 +267,9 @@ func (eds *ExtendedDataSquare) RowRoots() ([][]byte, error) {
 func deepCopy(original [][]byte) [][]byte {
 	dest := make([][]byte, len(original))
 	for i, cell := range original {
+		if cell == nil {
+			continue
+		}
 		dest[i] = make([]byte, len(cell))
 		copy(dest[i], cell)
 	}

extendeddatasquare_test.go
@@ -482,6 +482,23 @@ func TestRoots(t *testing.T) {
 	})
 }
 
+func TestDeepCopy(t *testing.T) {
+	original := make([][]byte, 16)
+	// fill first 8 shares with random data, leave the rest nil
+	for i := range original[:8] {
+		original[i] = make([]byte, 4)
+		_, err := rand.Read(original[i])
+		require.NoError(t, err)
+	}
+
+	copied := deepCopy(original)
+	require.Equal(t, original, copied)
+
+	// modify the original and ensure the copy is not affected
+	original[0][0]++
+	require.NotEqual(t, original, copied)
+}
+
 func createExampleEds(t *testing.T, shareSize int) (eds *ExtendedDataSquare) {
 	ones := bytes.Repeat([]byte{1}, shareSize)
 	twos := bytes.Repeat([]byte{2}, shareSize)